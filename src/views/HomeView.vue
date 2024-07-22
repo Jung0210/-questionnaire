c9b7e54fa0b815ebe74405a2c9dd47b846ebb7dc
Jung0210 <hsu870210@gmail.com>
2024-07-22 19:35:21 +1000

    榕駿0722 17:35

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script>
+
 </script>
 
 <template>
@@ -15,20 +16,28 @@
       <button class="press">搜尋</button>
     </div>
   </div>
+  <div class="Group18">
+
+  </div>
+  <div class="page">
+    <p>
+      << | < | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |> | >>
+    </p>
+  </div>
 </template>
-< style scoped lang="scss">
-  * {
+<style scoped lang="scss">
+* {
   box-sizing: border-box;
   margin: 0px;
   padding: 0px;
-  }
+}
 
-  // body {
-  // overflow-x: hidden;
-  // }
+body {
+  overflow-x: hidden;
+}
 
-  .searchArea {
-  width: 1520px;
+.searchArea {
+  width: 1500px;
   height: 150px;
   border: 1px solid rgb(0, 0, 0);
   display: flex;
@@ -37,41 +46,62 @@
   align-items: center;
 
   .enter {
-  width: 1300px;
-  height: 60px;
-  border: 1px solid rgb(0, 0, 0);
-  padding-top: 5px;
-  display: flex;
-  padding-top: 10px;
+    width: 1300px;
+    height: 60px;
+    border: 1px solid rgb(0, 0, 0);
+    padding-top: 5px;
+    display: flex;
+    padding-top: 10px;
 
-  input {
-  width: 50%;
-  height: 60%;
-  }
+    input {
+      width: 50%;
+      height: 60%;
+    }
 
-  .word {
-  font-size: 50px;
-  }
+    .word {
+      font-size: 50px;
+    }
 
-  .tree {
-  font-size: 20px;
-  }
+    .tree {
+      font-size: 20px;
+    }
   }
 
   .time {
-  width: 1300px;
-  height: 60px;
+    width: 1300px;
+    height: 60px;
+    border: 1px solid rgb(0, 0, 0);
+    font-size: 20px;
+    padding-top: 5px;
+    display: flex;
+    padding-top: 15px;
+
+    input {
+      width: 30%;
+      height: 50%;
+    }
+
+    button {
+      width: 10%;
+      height: 60%;
+    }
+  }
+}
+
+.Group18 {
+  width: 1500px;
+  height: 500px;
   border: 1px solid rgb(0, 0, 0);
-  font-size: 20px;
-  padding-top: 5px;
+  margin-top: 10px;
+}
+
+.page {
+  width: 714px;
+  height: 28px;
+  border: 1px solid rgb(0, 0, 0);
   display: flex;
-  padding-top: 15px;
-
-  input {
-  width: 30%;
-  height: 50%;
-  }
-  }
-  }
-
-  </style>
+  margin-left: 300px;
+  margin-top: 10px;
+  padding-left: 200px;
+}
+</style>
