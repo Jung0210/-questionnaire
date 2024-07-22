896eb2a4077ebda1609c708f11fbe550322e0dd1
Jung0210 <hsu870210@gmail.com>
2024-07-22 16:47:09 +1000

    榕駿0722 14:47

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="searchArea">
+
+  </div>
 </template>
+<style scoped lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+
+.searchArea {
+  width: 700px;
+  height: 500px;
+  border: 1px solid rgb(0, 0, 0);
+}
+</style>
