edb0086491ce3e58261eece8c2d3c69be168dac7
Jung0210 <hsu870210@gmail.com>
2024-07-22 18:00:49 +1000

    榕駿0722 16:00

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,14 @@
 
 <template>
   <div class="searchArea">
-
+    <div class="enter">
+      <p class="tree">問卷名稱:</p>
+      <input type="word" name="myfield" id="">
+    </div>
+    <div class="time">
+      <p>統計時間:</p>
+      <input type="text" id="" name="">
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
@@ -13,9 +20,41 @@
   padding: 0px;
 }
 
+body {
+  overflow-x: hidden;
+}
+
 .searchArea {
-  width: 700px;
-  height: 500px;
+  width: 1520px;
+  height: 150px;
   border: 1px solid rgb(0, 0, 0);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  .enter {
+    width: 1300px;
+    height: 60px;
+    border: 1px solid rgb(0, 0, 0);
+
+    .word {
+      font-size: 50px;
+    }
+
+    .tree {
+      font-size: 20px;
+
+    }
+
+  }
+
+  .time {
+    width: 1300px;
+    height: 60px;
+    border: 1px solid rgb(0, 0, 0);
+    font-size: 25px;
+    padding-top: 10px;
+
+  }
 }
 </style>
