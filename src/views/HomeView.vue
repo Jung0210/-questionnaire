142e08b9c5cf49e04f7215077620af68c0e0feb7
Jung0210 <hsu870210@gmail.com>
2024-07-22 18:53:03 +1000

    榕駿16:52 0722

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,52 +9,69 @@
     </div>
     <div class="time">
       <p>統計時間:</p>
-      <input type="text" id="" name="">
+      <input type="date" id="bookdate" name="">
+      <p>到</p>
+      <input type="date" id="bookdate" name="">
+      <button class="press">搜尋</button>
     </div>
   </div>
 </template>
-<style scoped lang="scss">
-* {
+< style scoped lang="scss">
+  * {
   box-sizing: border-box;
   margin: 0px;
   padding: 0px;
-}
+  }
 
-body {
-  overflow-x: hidden;
-}
+  // body {
+  // overflow-x: hidden;
+  // }
 
-.searchArea {
+  .searchArea {
   width: 1520px;
   height: 150px;
   border: 1px solid rgb(0, 0, 0);
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  align-items: center;
 
   .enter {
-    width: 1300px;
-    height: 60px;
-    border: 1px solid rgb(0, 0, 0);
+  width: 1300px;
+  height: 60px;
+  border: 1px solid rgb(0, 0, 0);
+  padding-top: 5px;
+  display: flex;
+  padding-top: 10px;
 
-    .word {
-      font-size: 50px;
-    }
+  input {
+  width: 50%;
+  height: 60%;
+  }
 
-    .tree {
-      font-size: 20px;
+  .word {
+  font-size: 50px;
+  }
 
-    }
-
+  .tree {
+  font-size: 20px;
+  }
   }
 
   .time {
-    width: 1300px;
-    height: 60px;
-    border: 1px solid rgb(0, 0, 0);
-    font-size: 25px;
-    padding-top: 10px;
+  width: 1300px;
+  height: 60px;
+  border: 1px solid rgb(0, 0, 0);
+  font-size: 20px;
+  padding-top: 5px;
+  display: flex;
+  padding-top: 15px;
 
+  input {
+  width: 30%;
+  height: 50%;
   }
-}
-</style>
+  }
+  }
+
+  </style>
