dc9af1ae9ad253fe67a1628d63b0566e7c63f7af
Jung0210 <hsu870210@gmail.com>
2024-07-25 22:19:45 +1000

    榕駿0725 20:19

--- src/views/BackEndInsidePage.vue
+++ src/views/BackEndInsidePage.vue
@@ -3,6 +3,46 @@
 </script>
 
 <template>
+    <div class="Group0">
+        <div class="circle1">
+            <p>問卷</p>
+        </div>
+        <div class="circle2">
+            <p>題目</p>
+        </div>
+        <div class="circle3">
+            <p>問卷回饋</p>
+        </div>
+        <div class="circle4">
+            <p>統計</p>
+        </div>
+    </div>
+    <div class="Group1">
+        <div class="oval1">
+            <p>問卷名稱:</p>
+            <input type="text">
+        </div>
+        <div class="oval2">
+            <p>問卷說明:</p>
+            <textarea name="" id=""></textarea>
+        </div>
+        <div class="oval3">
+            <p>開始時間:</p>
+            <input type="date" id="bookdate" name="">
+        </div>
+        <div class="oval4">
+            <p>結束時間:</p>
+            <input type="date" id="bookdate" name="">
+        </div>
+        <div class="Group2">
+            <div class="bean1">
+                <button class="Cancel">取消</button>
+            </div>
+            <div class="bean2">
+                <button class="next">下一步</button>
+            </div>
+        </div>
+    </div>
 </template>
 <style scoped lang="scss">
 * {
@@ -13,5 +53,186 @@
 
 body {
     overflow-x: hidden;
+}
+
+.Group0 {
+    width: 900px;
+    height: 50px;
+    // border: 1px solid rgb(0, 0, 0);
+    display: flex;
+    margin-left: 300px;
+    margin-top: 50px;
+
+    .circle1 {
+        width: 150px;
+        height: 50px;
+        border: 1px solid rgb(0, 0, 0);
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        background-color: #e2e2e2;
+        text-align: center;
+        font-size: 30px;
+
+        &:hover {
+            background-color: rgb(255, 255, 255);
+        }
+
+    }
+
+    .circle2 {
+        width: 150px;
+        height: 50px;
+        border: 1px solid rgb(0, 0, 0);
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        background-color: #e2e2e2;
+        text-align: center;
+        font-size: 30px;
+
+        &:hover {
+            background-color: rgb(255, 255, 255);
+        }
+    }
+
+    .circle3 {
+        width: 150px;
+        height: 50px;
+        border: 1px solid rgb(0, 0, 0);
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        background-color: #e2e2e2;
+        text-align: center;
+        font-size: 30px;
+
+        &:hover {
+            background-color: rgb(255, 255, 255);
+        }
+    }
+
+    .circle4 {
+        width: 150px;
+        height: 50px;
+        border: 1px solid rgb(0, 0, 0);
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        background-color: #e2e2e2;
+        text-align: center;
+        font-size: 30px;
+
+        &:hover {
+            background-color: rgb(255, 255, 255);
+        }
+
+    }
+}
+
+.Group1 {
+    width: 900px;
+    height: 800px;
+    border: 1px solid rgb(0, 0, 0);
+    margin-left: 300px;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    .oval1 {
+        width: 900px;
+        height: 200px;
+        // border: 1px solid rgb(0, 0, 0);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+        padding-right: 100px;
+
+        input {
+            width: 400px;
+            height: 50px;
+        }
+    }
+
+    .oval2 {
+        width: 900px;
+        height: 200px;
+        // border: 1px solid rgb(0, 0, 0);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+
+        textarea {
+            width: 502px;
+            height: 137px;
+            resize: none;
+        }
+    }
+
+    .oval3 {
+        width: 900px;
+        height: 200px;
+        // border: 1px solid rgb(0, 0, 0);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+        padding-right: 260px;
+
+        input {
+            font-size: 30px;
+        }
+    }
+
+    .oval4 {
+        width: 900px;
+        height: 200px;
+        // border: 1px solid rgb(0, 0, 0);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+        padding-right: 260px;
+
+        input {
+            font-size: 30px;
+        }
+
+    }
+
+    .Group2 {
+        width: 400px;
+        height: 50px;
+        // border: 1px solid rgb(0, 0, 0);
+        display: flex;
+        margin-left: 500px;
+
+        .bean1 {
+            width: 200px;
+            height: 50px;
+            // border: 1px solid rgb(0, 0, 0);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .Cancel {
+                width: 100px;
+                height: 30px;
+            }
+        }
+
+        .bean2 {
+            width: 200px;
+            height: 50px;
+            // border: 1px solid rgb(0, 0, 0);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .next {
+                width: 100px;
+                height: 30px;
+            }
+        }
+    }
+
 }
 </style>
